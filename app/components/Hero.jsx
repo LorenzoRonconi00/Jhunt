cb421e3c113635500beba6b91e92d1749a9b5b20
Federicocervelli <federicocervelli01@gmail.com>
2023-11-08 15:42:00 +0100

    tolta roba stupida scritta da quel porcodio di ritardato di lorenzo

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -2,33 +2,37 @@
 
 const Hero = () => {
   return (
-    <div className="z-10 h-screen flex flex-col justify-center items-center relative">
+    <div className="z-10 h-screen flex flex-col justify-start md:justify-center mt-[90px] md:mt-0 items-center relative px-5">
+      {/**Title */}
       <h1
-        className="text-5xl md:text-6xl lg:text-8xl font-bold"
+        className="text-7xl md:text-8xl font-bold mb-2"
         style={{ textShadow: "2px 2px 4px rgba(0,0,0,0.3)" }}
       >
         Jhunt.
       </h1>
-      <h2 className="text-lg md:text-4xl font-semibold text-center whitespace-normal break-words">
+      {/**Subtitles */}
+      <h2 className="text-2xl md:text-4xl  font-semibold text-center whitespace-normal break-words mb-5">
         L’assistente virtuale per la ricerca del tuo lavoro.
       </h2>
-      <h3 className="text-base md:text-2xl font-light text-center whitespace-normal break-words">
+      <h3 className="hidden md:block text-base md:text-2xl font-light text-center whitespace-normal break-words mb-10">
         Risparmia tempo e sforzi nella tua ricerca del lavoro con l'IA avanzata.
         <br />
         Trova opportunità su misura per te oggi!
       </h3>
+      {/**Search Bar */}
       <div className="flex flex-row justify-center items-center w-full max-w-2xl">
         <input
           type="text"
           placeholder="Cosa cerchi?"
-          className="w-full font-light border-2 border-[#7676767b] border-r-0 p-2"
+          className="w-full font-light border-2 border-[#7676767b] border-r-0 p-4"
         />
         <button className="bg-[#1F1695] h-full text-white font-bold text-center px-5 border-2 border-[#1F1695] border-l-0">
           Trova
         </button>
       </div>
-      <div className="flex flex-row justify-center items-center cursor-pointer">
-        <h3 className="text-lg md:text-3xl font-semibold text-[#F95225] mr-2 md:mr-4">
+      {/**Filtri */}
+      <div className="flex flex-row justify-center items-center cursor-pointer mt-5">
+        <h3 className="text-lg font-semibold text-[#F95225] mr-2">
           Filtri
         </h3>
         <img
