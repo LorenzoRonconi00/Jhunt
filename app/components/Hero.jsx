964d9ddd28b15694bbabd8e7fb59319127b4977f
LorenzoRonconi00 <lorenzoronconi60@gmail.com>
2023-11-09 13:05:28 +0100

    add blob animation footer

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,8 +1,33 @@
 "use client";
 
+import { ConfigProvider, Switch } from "antd";
+import { useState } from "react";
+
+
+const filtri = [
+  {
+    name: "Lavora da Remoto",
+  },
+  {
+    name: "Orario Flessibile",
+  },
+  {
+    name: "Sedi Internazionali"
+  }
+]
+
+
 const Hero = () => {
+
+  const [checked, setChecked] = useState(false);
+
+  const toggle = () => {
+    setChecked(!checked);
+  }
+
+
   return (
-    <div className="z-10 h-screen flex flex-col justify-start md:justify-center mt-[90px] md:mt-0 items-center relative px-5">
+    <div className="z-10 h-screen flex flex-col justify-start md:justify-center pt-[90px] md:pt-0 items-center relative px-5">
       {/**Title */}
       <h1
         className="text-7xl md:text-8xl font-bold mb-2"
@@ -12,10 +37,10 @@ const Hero = () => {
       </h1>
       {/**Subtitles */}
       <h2 className="text-2xl md:text-4xl  font-semibold text-center whitespace-normal break-words mb-5">
-        L’assistente virtuale per la ricerca del tuo lavoro.
+        Il tuo assistente virtuale per la ricerca del lavoro.
       </h2>
       <h3 className="hidden md:block text-base md:text-2xl font-light text-center whitespace-normal break-words mb-10">
-        Risparmia tempo e sforzi nella tua ricerca del lavoro con l'IA avanzata.
+        Risparmia tempo e sforzi nella tua ricerca del lavoro con la IA avanzata.
         <br />
         Trova opportunità su misura per te oggi!
       </h3>
@@ -24,15 +49,15 @@ const Hero = () => {
         <input
           type="text"
           placeholder="Cosa cerchi?"
-          className="w-full font-light border-2 border-[#7676767b] border-r-0 p-4"
+          className="w-full font-light border-2 border-[#7676767b] border-r-0 rounded-l-[10px] p-4 md:p-6"
         />
-        <button className="bg-[#1F1695] h-full text-white font-bold text-center px-5 border-2 border-[#1F1695] border-l-0">
+        <button className="bg-[#1F1695] h-full text-white text-[16px] md:text-[24px] font-bold text-center px-8 md:px-16 border-2 border-[#1F1695] rounded-r-[10px] border-l-0">
           Trova
         </button>
       </div>
       {/**Filtri */}
-      <div className="flex flex-row justify-center items-center cursor-pointer mt-5">
-        <h3 className="text-lg font-semibold text-[#F95225] mr-2">
+      <div className="flex flex-row justify-center items-center cursor-pointer mt-5" onClick={toggle}>
+        <h3 className="text-lg md:text-2xl font-semibold text-[#F95225] mr-2">
           Filtri
         </h3>
         <img
@@ -41,6 +66,28 @@ const Hero = () => {
           className="w-[18px] md:w-[22px]"
         />
       </div>
+      {checked && (
+        <div className="mt-3 md:mt-5">
+          <div className="w-[300px] h-[220px] md:w-[476px] md:h-[317px] bg-[#EBEBEB] border-[1px] border-black/20 flex flex-col justify-center rounded-[30px]">
+            {filtri.map((item, index) => (
+              <div key={index} className="flex flex-row justify-between px-10 py-3 md:py-5">
+                <h1 className="text-base md:text-xl font-semibold text-[#4F5E72]">{item.name}</h1>
+                <ConfigProvider
+                  theme={{
+                    token: {
+                      colorPrimary: "#1f1695",
+                    },
+                  }}>
+                  <Switch defaultChecked={false} />
+                </ConfigProvider>
+              </div>
+            ))}
+            <div className="mx-auto py-2 md:py-4 -mb-2 md:-mb-5">
+              <button className="w-24 h-8 md:w-32 md:h-10 bg-[#1f1695] text-white font-semibold text-lg rounded-[10px]" onClick={toggle}>Applica</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -15,8 +15,8 @@ const items = [
 
 const Navbar = () => {
   return (
-    <nav className="fixed z-50 top-0 w-screen flex justify-between items-center border-black/20 border-b py-2 px-5">
-      <div className="flex justify-start items-center gap-10">
+    <nav className="fixed z-50 top-0 w-screen flex justify-between items-center py-[20px] px-[40px] md:py-[40px] md:px-[60px]">
+      <div className="flex justify-start items-center gap-20">
         <div className="relative w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14">
           <Image
             src="/images/logo.png"
@@ -25,7 +25,7 @@ const Navbar = () => {
             objectFit="contain"
           />
         </div>
-        <div className=" hidden md:flex gap-10 md:font-bold md:text-black md:text-[24px] whitespace-nowrap">
+        <div className=" hidden md:flex gap-20 md:font-bold md:text-black md:text-[24px] whitespace-nowrap">
           {items.map((item, index) => (
             <a href={item.link} key={index}>
               {item.name}
@@ -35,7 +35,7 @@ const Navbar = () => {
       </div>
 
       <div className="flex items-center">
-        <button className="hidden md:block bg-transparent text-[#1f1695] py-3 px-8 border-[2px] border-black/20 font-semibold">
+        <button className="hidden md:block bg-transparent text-[#1f1695] py-3 px-8 border-[2px] border-black/20 rounded-[10px] font-semibold">
           ISCRIVITI
         </button>
         <img
