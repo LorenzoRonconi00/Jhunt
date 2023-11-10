8730ca1daa128f4631c44e2efea86de0435f80f6
LorenzoRonconi00 <lorenzoronconi60@gmail.com>
2023-11-10 01:51:18 +0100

    add button ACCEDI

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -16,7 +16,7 @@ const items = [
 const Navbar = () => {
   return (
     <nav className="fixed z-50 top-0 w-screen flex justify-between items-center py-[20px] px-[40px] md:py-[40px] md:px-[60px]">
-      <div className="flex justify-start items-center gap-20">
+      <div className="flex justify-start items-center lg:gap-20 md:gap-10">
         <div className="relative w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14">
           <Image
             src="/images/logo.png"
@@ -25,7 +25,7 @@ const Navbar = () => {
             objectFit="contain"
           />
         </div>
-        <div className=" hidden md:flex gap-20 md:font-bold md:text-black md:text-[24px] whitespace-nowrap">
+        <div className="hidden md:flex md:gap-10 lg:gap-20 md:font-bold md:text-black lg:text-[24px] md:text-[20px] whitespace-nowrap">
           {items.map((item, index) => (
             <a href={item.link} key={index}>
               {item.name}
@@ -34,8 +34,9 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="flex items-center">
-        <button className="hidden md:block bg-transparent text-[#1f1695] py-3 px-8 border-[2px] border-black/20 rounded-[10px] font-semibold">
+      <div className="flex items-center lg:gap-10">
+        <a href="#" className="hidden lg:block text-black font-bold text-[18px]">ACCEDI</a>
+        <button className="hidden md:block bg-transparent text-[#1f1695] lg:py-3 lg:px-8 py-2 px-4 border-[2px] border-black/20 rounded-[10px] font-semibold">
           ISCRIVITI
         </button>
         <img
