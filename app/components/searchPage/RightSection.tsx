144d22e3dac36f2d9614644ca716fb14ed5d4d46
LorenzoRonconi00 <lorenzoronconi60@gmail.com>
2023-11-14 01:10:21 +0100

    fixed layout

--- app/components/searchPage/RightSection.tsx
+++ app/components/searchPage/RightSection.tsx
@@ -1,37 +1,62 @@
 import SearchBox from "./SearchBox";
 import Suggestions from "./Suggestions";
 import Results from "./Results";
+import Image from "next/image";
 
 const RightSection = () => {
-  return (
-    <div
-      className="bg-white lg:h-full h-3/4 lg:w-3/4 w-full lg:top-0 bottom-0 fixed lg:right-0 lg:left-auto 
-        lg:rightSectionShadowLG rightSectionShadow"
-    >
-      <div className="w-full h-full">
-        <div className="flex flex-row justify-between py-5 px-5">
-          <div className="flex gap-5">
-            <div className="h-12 w-12 rounded-full bg-[black]"></div>
-            <div className="flex flex-col justify-center">
-              <h1 className="font-bold text-3xl">Cerca</h1>
+    return (
+        <div
+            className="bg-white lg:h-full rounded-tl-[40px] h-3/4 lg:w-3/4 w-full lg:top-0 bottom-0 fixed lg:right-0 lg:left-auto rightSectionShadowLG"
+        >
+            <div className="w-full h-full">
+                <div className="flex flex-row items-center justify-between py-10 px-5">
+                    <div className="flex gap-5">
+                        <div className="h-16 w-16 rounded-full bg-[#1F1695] mr-10"></div>
+                        <div className="flex flex-col justify-center">
+                            <h1 className="font-bold text-4xl">Cerca</h1>
+                        </div>
+                    </div>
+                    <div className="flex flex-row items-center">
+                        <div className="flex gap-32">
+                            <div className="relative h-[70px] w-[70px] rounded-full bg-[#bbbbbb] flex items-center justify-center">
+                                <Image
+                                    src="/images/messaggi.svg"
+                                    alt="Messaggi"
+                                    width={40}
+                                    height={40}
+                                    className="z-1"
+                                />
+                                <div className="h-[35px] w-[35px] bg-[#1F1695] rounded-full absolute top-0 right-0"></div>
+                            </div>
+                            <div className="h-[70px] w-[70px] rounded-full bg-[#bbbbbb] flex items-center justify-center">
+                                <Image
+                                    src="/images/notifiche.svg"
+                                    alt="Notifiche"
+                                    width={50}
+                                    height={40}
+                                />
+                            </div>
+                            <Image
+                                src="/images/profile.svg"
+                                alt="Profilo"
+                                width={70}
+                                height={70}
+                            />
+                        </div>
+                        <div className="flex flex-col px-7">
+                            <h1 className="text-left font-bold">Federico Cervelli</h1>
+                            <h2 className="text-left text-[#767676] text-[15px]">Lavoratore</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col h-full">
+                    <SearchBox />
+                    <Suggestions />
+                    <Results />
+                </div>
             </div>
-          </div>
-          <div className="flex">
-            <div className="flex flex-col mr-3">
-              <h1 className=" text-right">Lavoratore</h1>
-              <h2 className=" text-right font-bold">Federico Cervelli</h2>
-            </div>
-            <div className="h-12 w-12 rounded-full bg-[black]"></div>
-          </div>
         </div>
-        <div className="flex flex-col h-full">
-          <SearchBox />
-          <Suggestions />
-          <Results />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default RightSection;
