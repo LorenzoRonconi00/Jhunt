b34b79402fd3dff6c0bcca57a80f84112a90f20c
Federicocervelli <federicocervelli01@gmail.com>
2023-11-17 11:56:43 +0100

    added scroll

--- app/components/searchPage/Results.tsx
+++ app/components/searchPage/Results.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import { useState } from "react";
@@ -24,7 +24,8 @@ const results = [
     azienda: "Microsoft Team",
     ruolo: "Programmatore Junior C#",
     prezzo: "€ 55.000 - € 65.000",
-    descrizione: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis risus",
+    descrizione:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis risus",
     logo: "images/microsoft.svg",
     posizione: "Washington, America",
   },
@@ -32,7 +33,8 @@ const results = [
     azienda: "Microsoft Team",
     ruolo: "Programmatore Junior C#",
     prezzo: "€ 55.000 - € 65.000",
-    descrizione: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis risus",
+    descrizione:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis risus",
     logo: "images/microsoft.svg",
     posizione: "Washington, America",
   },
@@ -40,12 +42,12 @@ const results = [
     azienda: "Microsoft Team",
     ruolo: "Programmatore Junior C#",
     prezzo: "€ 55.000 - € 65.000",
-    descrizione: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis risus",
+    descrizione:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis risus",
     logo: "images/microsoft.svg",
     posizione: "Washington, America",
   },
-  
-]
+];
 
 function SearchBox() {
   const risultati = 5;
@@ -54,37 +56,57 @@ function SearchBox() {
 
   const handleClickColumns = () => {
     setClickedButtons(1);
-  }
+  };
 
   const handleClickRows = () => {
     setClickedButtons(2);
-  } 
+  };
 
   return (
     <>
-      <div className="w-full h-full pb-[110px] px-10 py-20">
-        <div className="flex flex-row items-center justify-between">
-          <h1 className="text-black text-[18px] font-bold">{risultati} risultati di annunci</h1>
-          <div className="flex flex-row items-center gap-x-5">
-            <button className="flex flex-row items-center justify-center gap-x-2 bg-white border-4 rounded-[30px] px-5 py-2">
-              <h1 className="text-[#1F1695] text-[18px] font-bold">Recenti</h1>
-              <Image src="images/arrow-blue.svg" alt="" width={20} height={20} />
-            </button>
-            <button 
+      <div className="flex flex-row items-center justify-between px-10 py-5">
+        <h1 className=" text-black text-[18px] font-bold">
+          {risultati} risultati di annunci
+        </h1>
+        <div className="flex flex-row items-center gap-x-5 ">
+          <button className="flex flex-row items-center justify-center gap-x-2 bg-white border-4 rounded-[30px] px-5 py-2">
+            <h1 className="text-[#1F1695] text-[18px] font-bold">Recenti</h1>
+            <Image src="images/arrow-blue.svg" alt="" width={20} height={20} />
+          </button>
+          <button
             onClick={handleClickColumns}
-            className={`flex items-center justify-center ${clickedButtons === 1 ? "border-2 border-[#1F1695]" : "border-0"} bg-[#DFF4FB] rounded-full w-[50px] h-[50px]`}>
-              <Image src="images/columns.svg" alt="" width={25} height={25}/>
-            </button>
-            <button 
+            className={`flex items-center justify-center ${
+              clickedButtons === 1 ? "border-2 border-[#1F1695]" : "border-0"
+            } bg-[#DFF4FB] rounded-full w-[50px] h-[50px]`}
+          >
+            <Image src="images/columns.svg" alt="" width={25} height={25} />
+          </button>
+          <button
             onClick={handleClickRows}
-            className={`flex items-center justify-center ${clickedButtons === 2 ? "border-2 border-[#1F1695]" : "border-0"} bg-[#DFF4FB] rounded-full w-[50px] h-[50px]`}>
-              <Image src="images/rows.svg" alt="" width={25} height={25}/>
-            </button>
-          </div>
+            className={`flex items-center justify-center ${
+              clickedButtons === 2 ? "border-2 border-[#1F1695]" : "border-0"
+            } bg-[#DFF4FB] rounded-full w-[50px] h-[50px]`}
+          >
+            <Image src="images/rows.svg" alt="" width={25} height={25} />
+          </button>
         </div>
-        <div className={`flex ${clickedButtons === 1 ? "flex-col" : "flex-row gap-x-5"} gap-y-5 py-5`}>
+      </div>
+
+      <div className="w-full h-full pb-[110px] px-10 py-5 overflow-y-scroll scrollbar-hide">
+        <div
+          className={`flex ${
+            clickedButtons === 1 ? "flex-col" : "flex-row gap-x-5"
+          } gap-y-5 py-5 `}
+        >
           {results.map((result, index) => (
-            <SingleResult azienda={result.azienda} ruolo={result.ruolo} prezzo={result.prezzo} descrizione={result.descrizione} logo={result.logo} posizione={result.posizione} key={index}
+            <SingleResult
+              azienda={result.azienda}
+              ruolo={result.ruolo}
+              prezzo={result.prezzo}
+              descrizione={result.descrizione}
+              logo={result.logo}
+              posizione={result.posizione}
+              key={index}
             />
           ))}
         </div>
